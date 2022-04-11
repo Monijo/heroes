b51057308f33b824ae67bea8f13197e18adbb1d7
Monijo <mokajozefowicz@gmail.com>
2022-04-11 22:16:09 +0200

    Add bake function and dom-functions.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
-module.exports={
-    printWidth: 80,
-    tabWidth:2,
-    bracketSpacing: true,
-    singleQuote: true,
-    trailingComma: "all",
+module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-import './styles/main.scss'
+import './styles/main.scss';
+import { showMessage } from './lib/dom';
+
+async function bake() {
+    const title = 'Baking cookies';
+    let counter = 0;
+
+    console.clear();
+    console.group(title);
+
+    counter++;
+    console.log(`${counter} - Add ingredients`);
+    showMessage(`${counter} - Add ingredients`, title);
+
+    counter++;
+    console.log(`${counter} - Mix ingredients`);
+    showMessage(`${counter} - Mix ingredients`, title, true);
+
+    const baking = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            counter++;
+            console.log(`${counter} - Bake at 325 degrees for 10 min`);
+            showMessage(
+                `${counter} - Bake at 325 degrees for 10 min`,
+                title,
+                true,
+            );
+            resolve(true);
+        }, 1000);
+    });
+
+    await baking;
+
+    counter++;
+    console.log(`${counter} - Eat ingredients`);
+    showMessage(`${counter} - Eat cookies`, title, true);
+    console.groupEnd();
+}
+
+// @ts-ignore
+document.querySelector('#bake-cookies').addEventListener('click', async () => {
+    bake();
+});
